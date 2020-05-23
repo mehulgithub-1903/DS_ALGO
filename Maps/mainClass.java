873773032ccfc3a35bea0144bb3ca4e3fc82e61c
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2020-05-23 11:45:35 +0530

    Update mainClass.java

--- Maps/mainClass.java
+++ Maps/mainClass.java
@@ -6,30 +6,29 @@ import java.util.Map.Entry;
 public class mainClass {
 
 	public static void main(String[] args) {
-		String s="sameena";
-		System.out.println(s.hashCode());
+		
 //	System.out.println(getHash("AB")%5);
-//	Map<String,Integer> numbers=new HashMap<>();
-//	numbers.put("Tom",125);
-//	numbers.put("Rich",968);
-//	numbers.put("Emily",789);
-//	numbers.put("Aaron",536);
-////numbers.put("Tom",745);
-//	numbers.putIfAbsent("two", 5);
-//	System.out.println(numbers.get("Emily"));
-//	System.out.println(numbers.entrySet());
-//	System.out.println(numbers.keySet());
-//	System.out.println(numbers.values());
-//	
-//	for(Entry<String,Integer> entry:numbers.entrySet()) {
-//	entry.setValue(entry.getValue()*18);	
-//	}
-//	OR YOU CAN MAKE A SET OF ENTRIES
-//	Set<Entry<String,Integer>> entries=numbers.entrySet();
-//	for(Entry<String,Integer>entry:entries) {
-//		entry.setValue(entry.getValue()*18);
-//	}
-//	System.out.println(numbers);
+	Map<String,Integer> numbers=new HashMap<>();
+	numbers.put("Tom",125);
+	numbers.put("Rich",968);
+	numbers.put("Emily",789);
+	numbers.put("Aaron",536);
+//numbers.put("Tom",745);
+	numbers.putIfAbsent("two", 5);
+	System.out.println(numbers.get("Emily"));
+	System.out.println(numbers.entrySet());
+	System.out.println(numbers.keySet());
+	System.out.println(numbers.values());
+	
+	for(Entry<String,Integer> entry:numbers.entrySet()) {
+	entry.setValue(entry.getValue()*18);	
+	}
+	OR YOU CAN MAKE A SET OF ENTRIES
+	Set<Entry<String,Integer>> entries=numbers.entrySet();
+	for(Entry<String,Integer>entry:entries) {
+		entry.setValue(entry.getValue()*18);
+	}
+	System.out.println(numbers);
 }
 
 	public static int getHash(String s) {
