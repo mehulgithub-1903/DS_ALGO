307182065ff06cbb2b7cdf34d0b4946cee660ee0
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2020-05-23 13:38:25 +0530

    23/05/2020

---
+++ anagrams/groupAnagrams.java
@@ -0,0 +1,32 @@
+package anagrams;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class groupAnagrams {
+
+	public static void main(String[] args) {
+      String [] words= {"eat", "tea", "tan", "ate", "nat", "bat"};
+		
+   solution sol =new solution();
+     System.out.println(sol.groupAnagram(words));		
+	}
+ static class  solution{
+	public List<List<String>> groupAnagram(String[] strs){
+		Map<String, List<String>> map=new HashMap<>();
+		for(String s:strs) {
+				char[] strArray=s.toCharArray();
+				Arrays.sort(strArray);
+				String sorted =new String(strArray);
+				if(!map.containsKey(sorted)) {
+				map.put(sorted, new LinkedList<>());	
+			}
+				map.get(sorted).add(s);
+		}
+		return new LinkedList<>(map.values());
+	}
+}
+}
